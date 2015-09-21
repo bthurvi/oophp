c18cae0ed3a4fb1635670ea25f1d3d2a3a3f5972
Urban Vikdahl <urban.vikdahl@skelleftea.se>
2015-09-21 21:33:34 +0200

    Skrev redovsningstexten för Kmom02

--- kmom02/webroot/pages/description.php
+++ kmom02/webroot/pages/description.php
@@ -14,3 +14,24 @@
 <p>Det gick bra för mig att inkludera source.php som en modul. först fick jag lov att lägga till en get-parameter i sökvägen för breadcrumben, mapparna och filerna eftersom jag använder mig av en index-sida med get parametrar. Jag fick även fixa lite med CSS-en för att få en snygg utskrift. </p>
   
 <p>Efter uppladdningen av av detta, kmom01, gjorde jag även extrauppgiften med GitHub - eftersom jag ville påminna mig om det. Jag hade gjort det förr, någon gång, men glömt en del. Det sägs ju att repetition är kunskapens moder...</p>
+
+<hr>
+
+<h4>Kmom02 - <a href="http://dbwebb.se/oophp/kmom02">Objektorienterad programmering i PHP</a></h4>
+
+<p>Jag känner ganska väl till objektorienterade koncept och programmeringssätt. Därför valde jag att endast skumläsa guiden oophp20 och sedan ge mig i kast med uppgifterna. Jag tror att jag har lyckats hyfsat med båda - men vi får se vad den som bedömmer mina lösningar säger...</p>
+
+<p>Tärningsspelet 100 skulle fungera så att en spelare kunde spela. Som extra uppgifter kunde man göra så att flera spelare kunde spela mot varandra, eller så att man kunde spela mot datorn. Jag valde att göra en lösning som förenar dessa krav på en gång genom att låta besökaren ange hur många mänskliga respektive datorspelare (AI:n) som skall spela.</p>
+
+<p>Utifrån användarens val skapas instanser av klasserna, CPlayer, C3Player, C15PointPlayer. Klassen CPlayer representerar en mänsklig spelare. Klasserna C3Player och C15PointPlayer ärver av klassen CPlayer samt utökar den med olika artificiell intelligens. Instanserna lagras sedan i en medlemsvariabel (array) i klassen CGame som representerar hela tärningsspelet. CGame har även en artificiell tärning i form av en instans av klassen C6Dice.</p>
+
+<p>För att webbplatsen skall kunna hålla reda på hela spelet mellan page-reloads så lagras hela spelets instans i en sessionsvariabel av <a href="?p=code&path=webroot/pages/dicegame.php">dicegame-sidan</a></p>
+
+<p>Samtliga klasser ligger sorterade i <a href="?p=code&path=src/dicegame/">dicegame-mapp</a>(de laddas av AutoLoaderns callback funktion).</p>
+
+<p>Jobbigast med själva uppgiften var faktiskt inte objektorienteringen utan var nog snarare meny och spellogiken. Personligen tycker jag att det ibland kan vara lite besvärligt att avgöra via if-satser och villkor när olika saker skall ske och inte.</p>
+
+<p>Kalenderuppgiften “månadens babe” (märkligt nästan sexsistiskt namn förrresten) var i mitt tycker något enklare. Den löste jag endast via två klasser: CCalendar och CsvMonths. CsvMonths innehåller endast svenska namn på månaderna (jag valde en sådan lösning istället för att förlita mig på locale-funktionen.) CCalendar håller reda på aktuell månad och år, plockar fram datumen samt skapar själva html-koden med bild och tabell.  Via lite kod från <a href="http://victor.se/bjorn/holidays.php">Björn Viktor</a> färgas dessutom  alla röda dagar under månaden/året.</p>
+
+<p>Sammanfattningsvis kan man säga att jag hade några få besvär,svårigheter och problem med logiken men löste det. Kursmomenten tycker jag har gett mig ökad erfarenhet i objektorienterad programmering Resultatet tycker jag blev hyfsat (även om koden säkerligen går att förfina, förenkla och göra elegantare). De lärdomar som dessa båda uppgifter har gett mig är ökad förståelse för variablers synlighet i samband med objektorienterad programmering (exempelvis parent:: och self::). Uppgifterna har även gett mig ökad erfarenhet av att få klass-instanser att samverka med varandra (som t.ex.C3Player och CDice). </p>
+
