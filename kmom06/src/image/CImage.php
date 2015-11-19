dd0ef4c3b29844da38583999c1608c125191c132
Urban Vikdahl <urban.vikdahl@skelleftea.se>
2015-11-19 10:43:26 +0100

    nu går bilden förminska

--- kmom06/src/image/CImage.php
+++ kmom06/src/image/CImage.php
@@ -9,6 +9,7 @@ class CImage
   private $verbose = false;
   private $mime;
   private $imgInfo;
+  private $image;
   private $attr;
   private $width;
   private $height;
@@ -17,8 +18,15 @@ class CImage
   private $pathToCache;
   private $quality;
   private $infoText='';
+  private $newWidth;
+  private $newHeight;
   
-  public function __construct($absolute_path_to_image_folder,$path_to_cachie_folder,$image_file,$instaniation_file_name=null, $quality=null) 
+  const MAX_WIDTH = 2000;
+  const MAX_HEIGHT = 2000;
+
+
+  public function __construct($absolute_path_to_image_folder,$path_to_cachie_folder,$image_file,$instaniation_file_name=null, $quality=null, 
+          $newWidth=null, $newHeight=null) 
   {  
     //save arguments
     $this->image_file=$image_file;
@@ -31,6 +39,9 @@ class CImage
     preg_match('#^[a-z0-9A-Z-_\.\/]+$#', $image_file) or $this->errorMessage('Filename contains invalid characters.');
     is_writable($path_to_cachie_folder) or $this->errorMessage('The cache dir is not a writable directory.');
     is_null($quality) or (is_numeric($quality) and $quality > 0 and $quality <= 100) or $this->errorMessage('Quality out of range');
+    is_null($quality) or (is_numeric($quality) and $quality > 0 and $quality <= 100) or $this->errorMessage('Quality out of range');
+    is_null($newWidth) or (is_numeric($newWidth) and $newWidth > 0 and $newWidth <= CImage::MAX_WIDTH) or $this->errorMessage('Width out of range');
+    is_null($newHeight) or (is_numeric($newHeight) and $newHeight > 0 and $newHeight <= CImage::MAX_HEIGHT) or $this->errorMessage('Height out of range');
     
     // Build full path to image
     $this->pathToImage = realpath($absolute_path_to_image_folder . $image_file);
@@ -40,12 +51,18 @@ class CImage
     substr_compare($absolute_path_to_image_folder, $this->pathToImage, 0, strlen($absolute_path_to_image_folder)) == 0 
     or $this->errorMessage('Security constraint: Source image must lie DIRECTLY under the image directory.');
     
+    //open file
+    $this->image = $this->openImage($this->pathToImage);
+    
     
     // Get information on the image
     $this->imgInfo  = getimagesize($this->pathToImage);
     !empty($this->imgInfo) or errorMessage("The file doesn't seem to be an image.");
     $this->mime = $this->imgInfo['mime'];
     list($this->width,$this->height,$this->type) = getimagesize($this->pathToImage);
+    
+    //resize (if needed)
+    $this->image = $this->newWidthHeight($newWidth, $newHeight, $this->image);
     
     //if old internal cashie file - create new one
     $this->cacheFileName = $this->createCasheFilename($path_to_cachie_folder, $quality);
@@ -54,11 +71,56 @@ class CImage
     if($imageModifiedTime > $cacheModifiedTime)
     {
       $this->infoText .= "None or old cache file - creating new one!<br/>";
-      $this->infoText .= $this->openInfo = $this->saveToCache($this->quality);
+      $this->saveToCache($this->image, $this->cacheFileName,$this->quality);
     }
-
    
+  }
+  
+  private function newWidthHeight($newWidth=null,$newHeight=null, $image=null)
+  {
+     
+    $aspectRatio = $this->width / $this->height;
+ 
     
+    if($newWidth && !$newHeight) 
+    {
+      $this->newWidth = $newWidth;
+      $this->newHeight = round($newWidth/$aspectRatio);
+      $this->infoText .= "New width is known $this->newWidth, height is calculated to $this->newHeight.<br/>"; 
+    }
+    else if(!$newWidth && $newHeight)
+    {
+      $this->newHeight = $newHeight;
+      $this->newWidth = round($newHeight * $aspectRatio);
+      $this->infoText .= "New height is known $newHeight, width is calculated to $newWidth.<br/>"; 
+    }
+    else if($newWidth && $newHeight) 
+   {
+      $ratioWidth  = $this->width  / $newWidth;
+      $ratioHeight = $this->height / $newHeight;
+      $ratio = ($ratioWidth > $ratioHeight) ? $ratioWidth : $ratioHeight;
+      $this->newWidth  = round($width  / $ratio);
+      $this->newHeight = round($height / $ratio);
+      $this->infoText .= "New width & height is requested, keeping aspect ratio results in {$this->newWidth}x{$this->newHeight}.<br/>"; 
+    }
+    else 
+    {
+      $this->newWidth  = $this->width;
+      $this->newHeight = $this->height;
+      $this->infoText .= "Keeping original width & heigth.<br/>"; 
+    }
+    
+    //create new size of image and return it
+    if(!($this->newWidth == $this->width && $this->newHeight == $this->height))
+    {
+      $imageResized = imagecreatetruecolor($this->newWidth, $this->newHeight);
+      imagecopyresampled($imageResized, $image, 0, 0, 0, 0, $this->newWidth, $this->newHeight, $this->width, $this->height);
+      $this->width  = $this->newWidth;
+      $this->height = $this->newHeight;
+      return $imageResized;
+    }
+    else
+      return $image;
   }
   
   private function createCasheFilename($cache_path, $quality)
@@ -73,14 +135,46 @@ class CImage
     return $cacheFileName;
   }
   
+  private function openImage($pathToImage)
+  {
+    $fileExtension  =  pathinfo($pathToImage,PATHINFO_EXTENSION);
+    
+    $this->infoText .= "File extension is: $fileExtension<br/>";
+    
+    //open image
+    switch($fileExtension)
+    {  
+      case 'jpg':
+      case 'jpeg': 
+        $image = imagecreatefromjpeg($pathToImage);
+        $this->infoText .= "Opened the image as a JPEG image<br/>";
+        break;  
+
+      case 'png':  
+        $image = imagecreatefrompng($pathToImage); 
+       $this->infoText .= "Opened the image as a PNG image.<br/>";
+        break;  
+
+      default: $this->infoText .= "No support for this file extension.<br/>";    
+    }
+    
+    return $image;   
+  }
+  
+  private function saveToCache($image,$cacheFileName,$quality=100)
+  {
+     imagejpeg($image, $cacheFileName, $quality);
+     $this->infoText .= "Saved image as JPEG to cache using quality = $quality<br/>";
+  }
  
   
-  private function saveToCache($quality)
+ /* private function saveToCache($quality,$newWidth,$newHeight)
   {
     $fileExtension  =  pathinfo($this->pathToImage,PATHINFO_EXTENSION);
     
     $info = "File extension is: $fileExtension<br/>";
-
+    
+    //open image
     switch($fileExtension)
     {  
       case 'jpg':
@@ -96,13 +190,17 @@ class CImage
 
       default: $info .= "No support for this file extension.<br/>";    
     }
+    
+    //resize (if needed)
+    $image = $this->newWidthHeight($newWidth, $newHeight, $image);
+    
 
-  
+    //save image
      imagejpeg($image, $this->cacheFileName, $quality);
       $info .= "Saved image as JPEG to cache using quality = $quality<br/>";
      
      return $info;
-  }
+  }*/
 
   
   
@@ -139,12 +237,14 @@ class CImage
       // has  the browser correct version of the image in cache?
       if($browserCacheTime == $internalCacheTime)
       {
-        header('HTTP/1.0 304 Not Modified');       
+        header('HTTP/1.0 304 Not Modified');
+        //var_dump("Browser cache:". $this->cacheFileName);
       }
       else
       {
         header('Content-type: ' . $this->mime);
         readfile($this->cacheFileName);
+        //var_dump("Internal cache:".$this->cacheFileName);
       }
     }
     else 
@@ -152,7 +252,12 @@ class CImage
       clearstatcache();
       
       //fetch som info about the image
-      $url =  "./img.php?src=$this->image_file"."&amp;quality=$this->quality" ; 
+      $url =  "./img.php" ; 
+      parse_str($_SERVER['QUERY_STRING'], $query);
+      unset($query['verbose']);
+      $url .= '?' . http_build_query($query);
+      
+      
       $imgInfo =  print_r($this->imgInfo, true);     
       $filesize = filesize($this->pathToImage);
       $memory_peak = round(memory_get_peak_usage() /1024/1024);
--- kmom06/webroot/pages/img.php
+++ kmom06/webroot/pages/img.php
@@ -10,6 +10,8 @@ $verbose  = isset($_GET['verbose']) ? true              : null;
 $quality  = isset($_GET['quality']) ? $_GET['quality']  : 60;
 $ignoreCache = isset($_GET['no-cache']) ? true : false;
 //$saveAs   = isset($_GET['save-as']) ? $_GET['save-as']  : null; - not needed acording to MOS...
+$newWidth   = isset($_GET['width'])   ? $_GET['width']    : null;
+$newHeight  = isset($_GET['height'])  ? $_GET['height']   : null; 
 
 
 //require src attribute
@@ -19,7 +21,7 @@ isset($src) or die('Must set src-attribute.');
 //create instance of class and ouptu image
 $imgFolder = dirname(__DIR__).DIRECTORY_SEPARATOR .'img'. DIRECTORY_SEPARATOR;
 $cacheFolder = dirname(__DIR__).DIRECTORY_SEPARATOR .'cache'. DIRECTORY_SEPARATOR;
-$cimg = new CImage($imgFolder, $cacheFolder , $src, __FILE__,$quality);
+$cimg = new CImage($imgFolder, $cacheFolder , $src, __FILE__,$quality,$newWidth,$newHeight);
 $cimg->outputImage($ignoreCache,$verbose);
 
 
