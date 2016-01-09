6d2c58242514e75e2080bbc8cb00d83ab2436903
Urban Vikdahl <urban.vikdahl@skelleftea.se>
2016-01-09 19:42:47 +0100

    startsidan ok

--- kmom0710/src/base/CStart.php
+++ kmom0710/src/base/CStart.php
@@ -10,14 +10,22 @@
 class CStart 
 {
   private $dbh = null;
+  private $textfilter = null;
   private $latest3movies=null;
   private $genres=null;
+  private $popular=null;
+  private $lastRent=null;
+  private $blogs=null;
   
   public function __construct($dbSettings) 
   { 
     $this->dbh = new CDatabase($dbSettings);
+    $this->textfilter = new CTextFilter();
     $this->genres = CMovieSearch::getGenres($this->dbh,"movies");
     $this->latest3movies = $this->get3latestMovies();
+    $this->popular = $this->getMostPopularMovie();
+    $this->lastRent = $this->getLastRentMovie();
+    $this->blogs = $this->get3blogs();
   }
   
   public function getHtml(){
@@ -26,7 +34,34 @@ class CStart
    foreach ($this->latest3movies as $mov) {
      $movieImg[]= substr($mov->bild, strpos($mov->bild, '/') + 1);
    }
-
+   $movieImg[]= substr($this->popular[0]->bild, strpos($this->popular[0]->bild, '/') + 1);
+   $movieImg[]= substr($this->lastRent[0]->bild, strpos($this->lastRent[0]->bild, '/') + 1);
+   
+   $display = null;
+   foreach ($this->blogs as $post)
+   {
+     $title  = ucfirst(htmlentities($post->title, null, 'UTF-8'));
+     
+     $data   = $this->textfilter->doFilter(htmlentities($post->data, null, 'UTF-8'), $post->filter);
+     $result = preg_split('/(?<=[.?!])\s+/', $data, -1, PREG_SPLIT_NO_EMPTY);
+        
+        if(array_key_exists(0,$result))
+        {
+          $result = preg_split('/<br[^>]*>/i', $result[0]);
+          $result = $result[0];
+        }
+        
+       if(empty($result))
+         $result = "Text saknas.";
+       
+       $result .= " <a href='?p=blogg&amp;slug={$post->slug}'>Läs mer »</a>";
+     
+     
+     $display .= "<div class='blogpost'>\n"
+                  . "<h2><a href='?p=blogg&amp;slug={$post->slug}'>$title</a></h2>"
+                   . "<p>$result</p>"
+                 . "</div>";
+   }
    
   
     return <<< EOD
@@ -36,30 +71,35 @@ class CStart
                 </div>
                 <div class="start">
                     <div>
-                        <div class="cols2">
-                          Mest populära film 
+                        <div class="cols2" style='float:left;'>
+                          <div class="heading">Mest populära film:</div>
+                          <img src='pages/img.php?src={$movieImg[3]}&amp;width=400&amp;height=150&amp;crop-to-fit' alt='en bild'/><br/>
+                          {$this->popular[0]->titel}
                         </div>
                         <div class="cols2">
-                          Senast hyrda film
+                          <div class="heading">Senast hyrda film:</div>
+                          <img src='pages/img.php?src={$movieImg[4]}&amp;width=400&amp;height=150&amp;crop-to-fit' alt='en bild'/><br/>
+                          {$this->lastRent[0]->titel}
                         </div>
                     </div>
                     <div class="newFilms">
                         <div class="heading">Nyaste filmerna:</div>
                        <div class="cols3">
-                          <img src='pages/img.php?src={$movieImg[0]}&amp;width=400&amp;height=250&amp;crop-to-fit' alt='en bild'/><br/>
+                          <img src='pages/img.php?src={$movieImg[0]}&amp;width=400&amp;height=150&amp;crop-to-fit' alt='en bild'/><br/>
                           {$this->latest3movies[0]->titel}
                        </div>
                        <div class="cols3">
-                          <img src='pages/img.php?src={$movieImg[1]}&amp;width=400&amp;height=250&amp;crop-to-fit' alt='en bild'/><br/>
+                          <img src='pages/img.php?src={$movieImg[1]}&amp;width=400&amp;height=150&amp;crop-to-fit' alt='en bild'/><br/>
                           {$this->latest3movies[1]->titel}
                        </div>
                        <div class="cols3">
-                          <img src='pages/img.php?src={$movieImg[2]}&amp;width=400&amp;height=250&amp;crop-to-fit' alt='en bild'/><br/>
+                          <img src='pages/img.php?src={$movieImg[2]}&amp;width=400&amp;height=150&amp;crop-to-fit' alt='en bild'/><br/>
                           {$this->latest3movies[2]->titel}
                        </div>
                     </div>
                     <div class="latestBlogs">
-                      De tre senaste blogginläggen
+                      <div class="heading">Senaste blogginlägg:</div>
+                      {$display}
                     </div>
                 </div>
 EOD;
@@ -77,6 +117,48 @@ EOD;
         ORDER BY creationdate
         LIMIT 3";
      
+    $res = $this->dbh->ExecuteSelectQueryAndFetchAll($sql);
+    
+    return $res;
+  }
+  
+  private function getMostPopularMovie(){
+    
+     $sql = "SELECT M.id as id, M.title as titel, I.image as bild
+      FROM oophp0710_movie AS M
+        INNER JOIN oophp0710_movie2image AS M2I
+          ON M2I.movie_id = M.id
+        INNER JOIN oophp0710_images AS I
+          ON M2I.image_id = I.id
+        WHERE M.id=7
+        LIMIT 1";
+     
+     $res = $this->dbh->ExecuteSelectQueryAndFetchAll($sql);
+    
+    return $res;
+  }
+  private function getLastRentMovie(){
+    
+    $sql = "SELECT M.id as id, M.title as titel, I.image as bild
+      FROM oophp0710_movie AS M
+        INNER JOIN oophp0710_movie2image AS M2I
+          ON M2I.movie_id = M.id
+        INNER JOIN oophp0710_images AS I
+          ON M2I.image_id = I.id
+        WHERE M.id=4
+        LIMIT 1";
+     
+     $res = $this->dbh->ExecuteSelectQueryAndFetchAll($sql);
+    
+    return $res;
+    
+  }
+  
+  private function get3blogs(){
+    
+    $sql = "SELECT * FROM oophp0710_content WHERE type = 'post' AND published <= NOW() AND deleted IS NULL ORDER BY updated DESC LIMIT 3;";
+    
+    
     $res = $this->dbh->ExecuteSelectQueryAndFetchAll($sql);
     
     return $res;
